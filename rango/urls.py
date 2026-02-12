fbae267f5aba379b3b6c5566fb8537324d30ae8c
“Illya <3071154s@student.gla.ac.uk>
2026-02-12 04:37:09 +0000

    Chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
