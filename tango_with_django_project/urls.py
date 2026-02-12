b47401d34792d156ca7483840d88c7d757b46b50
“Illya <3071154s@student.gla.ac.uk>
2026-02-12 21:39:27 +0000

    Complete Chapter 4 exercises

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 from django.contrib import admin
 from django.urls import path
@@ -24,5 +25,7 @@ from django.urls import include
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
